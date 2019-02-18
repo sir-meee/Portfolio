2c24df03951a0cb2c7bc16d58d52ab1378e1dc5d
sir-meee <sammymbevi@gmail.com>
2019-02-18 18:46:38 +0300

    portfolio

---
+++ scripts.js
@@ -0,0 +1,101 @@
+// Browser support
+function currentYPosition() {
+  // Firefox, Chrome, Opera, Safari
+  if (self.pageYOffset) {
+    return self.pageYOffset;
+  }
+  // Internet Explorer 6 - standards mode
+  if (document.documentElement && document.documentElement.scrollTop) {
+    return document.documentElement.scrollTop;
+  }
+  // Internet Explorer 6, 7 and 8
+  if (document.body.scrollTop) {
+    return document.body.scrollTop;
+  }
+
+  return 0;
+}
+
+// Determine the position of the destination element
+function elmYPosition(eID) {
+  var elm = document.querySelector(eID);
+  var y = elm.offsetTop;
+  var node = elm;
+  while (node.offsetParent && node.offsetParent != document.body) {
+    node = node.offsetParent;
+    y += node.offsetTop;
+  }
+  return y;
+}
+
+// Function to do the scrolling
+function smoothScroll(eID) {
+  var startY = currentYPosition();
+  var stopY = elmYPosition(eID);
+  var distance = stopY > startY ? stopY - startY : startY - stopY;
+  if (distance < 100) {
+    scrollTo(0, stopY);
+    return;
+  }
+  var speed = Math.round(distance / 100);
+  if (speed >= 20) speed = 20;
+  var step = Math.round(distance / 25);
+  var leapY = stopY > startY ? startY + step : startY - step;
+  var timer = 0;
+  if (stopY > startY) {
+    for (var i = startY; i < stopY; i += step) {
+      setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+      leapY += step;
+      if (leapY > stopY) leapY = stopY;
+      timer++;
+    }
+    return;
+  }
+  for (var i = startY; i > stopY; i -= step) {
+    setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+    leapY -= step;
+    if (leapY < stopY) leapY = stopY;
+    timer++;
+  }
+  return false;
+}
+
+// Triggering scroll function
+document.querySelector(".user-nav").addEventListener("click", function(event) {
+  var target = event.target;
+  var anchorID = target.getAttribute("href");
+
+  if (target.nodeName === "I" || target.nodeName === "SPAN") {
+    anchorID = target.parentElement.getAttribute("href");
+  }
+
+  smoothScroll(anchorID);
+});
+
+document
+  .querySelector(".footer-nav")
+  .addEventListener("click", function(event) {
+    var target = event.target;
+    var anchorID = target.getAttribute("href");
+    smoothScroll(anchorID);
+  });
+
+document.querySelector("#cta").addEventListener("click", function() {
+  smoothScroll("#portfolio");
+});
+
+// Submit form
+document.querySelector(".form__submit").addEventListener("click", function() {
+  var formInputs = document.querySelectorAll(".form__input");
+
+  for (var i = 0; i < formInputs.length; i++) {
+    if (formInputs[i].value === "" && formInputs[i].hasAttribute("required")) {
+      return false;
+    }
+  }
+  document.querySelector(".expand").classList.add("show-expand");
+});
+
+document.querySelector(".close-btn").addEventListener("click", function() {
+  document.querySelector(".expand").classList.remove("show-expand");
+});
